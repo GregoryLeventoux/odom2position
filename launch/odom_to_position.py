2745c9c2299d72c5bfc3818ab9e01e83eed32f91
Grégory <gregory.leventoux@hotmail.fr>
2022-03-30 17:08:25 +0200

    m

--- launch/odom_to_position.py
+++ launch/odom_to_position.py
@@ -4,12 +4,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='odom2position',
-            executable='odom2position',
+            package='odom_to_position',
+            executable='odom_to_position',
             name='viewer1',
             parameters=[{"subscribed_topic": "/r2d2/odom"},{"published_topic": "/real_position"}]),
         Node(
-            package='odom2position',
-            executable='odom2position',
+            package='odom_to_position',
+            executable='odom_to_position',
             name='viewer2',
             parameters=[{"subscribed_topic": "/odometry/filtered"},{"published_topic": "/position/filtered"}])])
